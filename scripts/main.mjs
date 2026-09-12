eae3d0fb098fc9dac35a8e307e5ebd4055b2ed6f
pythrz <pythrr@gmail.com>
2026-09-12 15:37:56 -0400

    Update main.mjs
    - Fix bug when trying to collapse a category for the first time in a journal you've collapsed a different category in

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -576,6 +576,7 @@ Hooks.on("renderPythrJournal", (app, html, context, options) => {
 			heading.classList.toggle('colactive');
 			var content = heading.nextElementSibling
 			var keys = app.document.getFlag(MODULE_ID, collapse_key) ?? {[id]: {[game.user.id]: true}}
+			if( !(id in keys) ) keys[id] = { [game.user.id]: true };
 			if( typeof keys[id] === "boolean" ) keys[id] = { [game.user.id]: true } // If using the old method of storing data, update it.
 			
 			if( heading.classList.contains('colactive') ) {
